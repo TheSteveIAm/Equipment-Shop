59848800dd84928d801bf07dd83c1dcd099315bd
TheSteveIAm <TheSteveIAm@Gmail.com>
2018-01-24 22:29:25 -0600

    Added plugins: -Probuilder (for quick prototype object creation) -Postprocessing Stack (to frame Matt's future art assets in a beautiful light!) -Cinemachine (camera goodness)
    Added first test scene, includes:
    -Movable player
    -Some blockout terrain
    -Cinemachine Camera

---
+++ Assets/Scripts/Characters/Player/Movement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    //private Animator anim;
+    public float speed = 1f;
+    //Rigidbody body;
+
+    //void Start()
+    //{
+    //    //anim = GetComponentInChildren<Animator>();
+    //    //body = GetComponent<Rigidbody>();
+
+    //}
+
+    public void MoveDelta(Vector3 dir)
+    {
+        //anim.SetFloat("Speed", (restrictMovement) ? 0f : dir.magnitude);
+
+        transform.position += dir * speed * Time.deltaTime;
+        //body.MovePosition(body.position + dir * speed * Time.deltaTime);
+        if (dir.magnitude > 0.1f)
+        {
+            transform.LookAt(transform.position + dir);
+        }
+
+    }
+}
---
+++ Assets/Scripts/Characters/Player/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    Movement move;
+
+    Vector3 targetDir;
+
+	// Use this for initialization
+	void Start () {
+        move = GetComponent<Movement>();
+	}
+
+    //void Update()
+    //{
+    //}
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        targetDir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        move.MoveDelta(targetDir * 7.5f);
+    }
+}
