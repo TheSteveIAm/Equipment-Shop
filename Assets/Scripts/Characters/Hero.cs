add230471121c795148490d9c6d640bbe2618925
TheSteveIAm <TheSteveIAm@Gmail.com>
2018-02-02 23:20:09 -0600

    Added base hero movement, hero finds points of interest, and can walk to them. Hero can also recognize stations they're standing at

--- Assets/Scripts/Characters/Hero.cs
+++ Assets/Scripts/Characters/Hero.cs
@@ -1,20 +1,98 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class Hero : Character {
+using UnityEngine.AI;
+public class Hero : Character
+{
 
     private Inventory inventory;
+    private Item carriedItem;
+    private NavMeshAgent agent;
 
-	// Use this for initialization
-	protected override void Start () {
+    private PointOfInterest[] points;
+    //private PointOfInterest currentInterest;
+
+    [Range(1, 4)]
+    private float lingerTime = 1f;
+    private float lingerTimer;
+
+    private bool lingering;
+    private Station currentStation;
+
+    // Use this for initialization
+    protected override void Start()
+    {
         base.Start();
 
+        agent = GetComponent<NavMeshAgent>();
+
         inventory = GetComponent<Inventory>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+        points = FindObjectsOfType<PointOfInterest>();
+
+        //test
+        ChoosePointOfInterest(POIType.Item);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (lingering)
+        {
+            lingerTimer += Time.deltaTime;
+
+            if (lingerTimer >= lingerTime)
+            {
+                lingering = false;
+                lingerTimer = 0f;
+                ChoosePointOfInterest();
+            }
+            return;
+        }
+
+        if (Vector3.Distance(transform.position, agent.destination) <= 1.1f)
+        {
+            lingering = true;
+            //lingerTime = Random.Range(0f, 4f);
+            lingerTime = 1;
+        }
+
+    }
+
+    void ChoosePointOfInterest(POIType poi)
+    {
+
+        List<PointOfInterest> pointsOfSelectedType = new List<PointOfInterest>();
+
+        for (int i = 0; i < points.Length; i++)
+        {
+            if (points[i].navPointOfInterest == poi)
+            {
+                pointsOfSelectedType.Add(points[i]);
+            }
+
+        }
+
+        agent.destination = pointsOfSelectedType[Random.Range(0, pointsOfSelectedType.Count - 1)].transform.position;
+    }
+
+    void ChoosePointOfInterest()
+    {
+        agent.destination = points[Random.Range(0, points.Length - 1)].transform.position;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Station station = other.GetComponent<Station>();
+        
+        if(station != null)
+        {
+            currentStation = station;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        currentStation = null;
+    }
 }
---
+++ Assets/Scripts/Characters/NPCs/PointOfInterest.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum POIType
+{
+    Item,
+    Trade
+}
+
+
+public class PointOfInterest : MonoBehaviour {
+
+    public POIType navPointOfInterest;
+}
